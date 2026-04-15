c1a794ba68b3067fd41556a9956908fc90d92209
Joseph Hindmarsh <joe.hindmarsh21@gmail.com>
2026-04-15 22:35:51 +0100

    change writer to global to fix missing context

--- js/serial-test.js
+++ js/serial-test.js
@@ -1,8 +1,8 @@
 document.getElementById("result").innerHTML = "Changed by JS"
 document.getElementById("btnConnect").addEventListener("click", getSerial)
 document.getElementById("btnMsg").addEventListener("click", writeSerial)
-
-
+document.getElementById("btnMsg").addEventListener("click", writeSerial2)
+let writer;
 
 async function getSerial() {
     // 1. Request a port from the user
@@ -21,14 +21,17 @@ async function getSerial() {
     // const readableStreamClosed = textEncoder.readable.pipeTo(port.writable);
     
     
-    const writer = textEncoder.writable.getWriter();
+    writer = textEncoder.writable.getWriter();
     console.log("saying hello")
-    while(true){
-        await writer.write("hello\n");
-    }
+
+    // while(true){
+    //     await writer.write("hello\n");
+    //     await new Promise((res) => setTimeout(() => res(), 1000))
+    //     writer.releaseLock();
+    // }
     
     console.log("done?")
-    writer.releaseLock();
+    // writer.releaseLock();
     // await port.close();
 
 
@@ -46,19 +49,13 @@ async function getSerial() {
 
 async function writeSerial(){
     // const writer = outputStream.getWriter();
+    consele.log("write func 1");
     writer.write("hello from the web!\n");
     writer.releaseLock();
-
-
-    // // 3. Set up a writer to send data
-    // const writer = port.writable.getWriter();
-    // const data = new TextEncoder().encode("Hello Device!");
-    // await writer.write(data);
-    
-    // // 4. Release the lock when done
-    // writer.releaseLock();
 }
 
+
+// This doesn't do anything other than print the ports to the console.
 navigator.serial.getPorts().then((ports) => {
   // Initialize the list of available ports with `ports` on page load.
   console.log(`available ports= ${ports}`)
